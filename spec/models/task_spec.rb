98ef3e3f6eab31b1e9ebd7aaad9163308e08d16f
sandrinenikuze <nikuzesandrine@yahoo.fr>
2017-03-07 10:16:35 +0200

    testing homework

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -14,4 +14,39 @@ RSpec.describe Task, type: :model do
       expect(task.complete).to eq(true)
     end
   end
+  describe '#toggle_favorite!' do 
+    it "should switch favorite to false if it began as true" do
+      task = Task.new(favorite: true)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(false)
+    end
+
+    it "should switch favorite to true if it began as false" do 
+      task = Task.new(favorite: false)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(true)
+    end
+  end
+  describe 'overdue?' do 
+    it "should return true if Time.now > deadline!" do
+      task = Task.new(deadline: Time.now+ 10.hours)
+      expect(task.overdue?).to eq(false)
+    end
+
+    it "should return false if Time.now < deadline" do
+      task = Task.new(deadline: Time.now- 10.hours)
+      expect(task.overdue?).to eq(true)
+    end
+  end
+  describe 'increment_priority!' do 
+    it "should return true if Time.now > deadline!" do
+      task = Task.new(deadline: Time.now+ 10.hours)
+      expect(task.overdue?).to eq(false)
+    end
+
+    it "should return false if Time.now < deadline" do
+      task = Task.new(deadline: Time.now- 10.hours)
+      expect(task.overdue?).to eq(true)
+    end
+  end
 end
